08481071238fa6b616f7a1f2095118b697ad171d
Senna de Vos <senna.devos@outlook.com>
2025-04-07 17:48:26 +0200

    feat: added an alert that informs the visitor, the page is a work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
 import HeroSection from "./sections/HeroSection.vue";
+import { onMounted } from "vue";
+
+import { useI18n } from "vue-i18n";
+const { t } = useI18n()
+
+onMounted(() => {
+  alert(t('wip_message'))
+})
 </script>
 
 <template>
