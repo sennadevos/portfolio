a5286842c3d0482b61a4a50f318b9433672b7a08
Senna de Vos <senna.devos@outlook.com>
2025-05-12 18:37:24 +0200

    feat(help): add a `Need Help?` section

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import HeroSection from "./sections/HeroSection.vue";
 import Footer from "./sections/Footer.vue";
 import AboutSection from "./sections/AboutSection.vue";
+import HelpSection from "./sections/HelpSection.vue";
 </script>
 
 <template>
   <div>
     <HeroSection />
     <AboutSection />
+    <HelpSection />
     <Footer />
   </div>
 </template>
